5f8d0a2aa1624a6eef9d67e03e23aee4f1b829ad
番提 <w915546302@gmail.com>
2022-10-07 11:42:01 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-bold underline">Hello CodeSandbox</div>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
